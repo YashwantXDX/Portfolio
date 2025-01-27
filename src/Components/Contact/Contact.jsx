008b80d5ecd5bd3ad95fb157264fa67e06dd7004
YashwantXDX <yashwant.choure73546@gmail.com>
2025-01-27 12:57:39 +0530

    Contact Form Functionality Completed

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,6 +6,34 @@ import location_icon from '../../assets/location_icon.svg';
 import call_icon from '../../assets/call_icon.svg';
 
 const Contact = () => {
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        
+        // For Future Myself, This is the access key from which I can get email from the contact form.
+
+        // This functionality is from web3forms.com
+
+        formData.append("access_key", "370a1443-c879-48d6-bf8d-f41350bb269e");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          alert(res.message);
+        }
+    }
+
   return (
     <div id='contact' className='contact'>
         <div className="contact-title">
@@ -31,7 +59,7 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-            <form className="contact-right">
+            <form onSubmit={onSubmit} className="contact-right">
                 <label htmlFor="">Your Name</label>
                 <input type="text" placeholder='Enter your name'
                 name='name'/>
