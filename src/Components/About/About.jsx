fb62fb079202aada9bd7f06fd3a4b4b604c6b8ec
YashwantXDX <yashwant.choure73546@gmail.com>
2025-01-27 21:49:22 +0530

    Added Profiles of Leetcode,gfg and linkedin

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,65 +2,79 @@ import React from 'react';
 import './About.css';
 import theme_pattern from '../../assets/theme_pattern.svg';
 import profile_img from '../../assets/profile_img.svg';
+import linkedin_logo from '../../assets/linkedin_logo.svg';
+import leetcode_logo from '../../assets/leetcode_logo.svg';
+import gfg_logo from '../../assets/geekforgeeks_logo.svg';
 
 const About = () => {
   return (
     <div id='about' className='about'>
-        <div className="about-title">
-            <h1>About me</h1>
-            <img src={theme_pattern} alt="" />
+      <div className="about-title">
+        <h1>About me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={profile_img} alt="" />
         </div>
-        <div className="about-sections">
-            <div className="about-left">
-                <img src={profile_img} alt="" />
+        <div className="about-right">
+          <div className="about-para">
+            <p>I am a Software Developer with hands on skills on Java, C++, Dart, Javascript. Throughout my learnings, I have made several projects which shows my passion to make robust apps.</p>
+            <p>My passion for Software Development is not only reflected in my extensive experience but also in the enthusiasm and dedication I bring to each project.</p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>Flutter & Dart</p>
+              <hr style={{ width: "50%" }} />
             </div>
-            <div className="about-right">
-                <div className="about-para">
-                    <p>I am a Software Developer with hands on skills on Java, C++, Dart, Javascript. Throughout my learnings, I have made several projects which shows my passion to make robust apps.</p>
-                    <p>My passion for Software Development is not only reflected in my extensive experience but also in the enthusiasm and dedication I bring to each project.</p>
-                </div>
-                <div className="about-skills">
-                    <div className="about-skill">
-                        <p>Flutter & Dart</p>
-                        <hr style={{width: "50%"}}/>
-                    </div>
-                    <div className="about-skill">
-                        <p>Java</p>
-                        <hr style={{width: "70%"}}/>
-                    </div>
-                    <div className="about-skill">
-                        <p>React.js</p>
-                        <hr style={{width: "40%"}}/>
-                    </div>
-                    <div className="about-skill">
-                        <p>MySQL</p>
-                        <hr style={{width: "60%"}}/>
-                    </div>
-                    <div className="about-skill">
-                        <p>Spring Boot</p>
-                        <hr style={{width: "30%"}}/>
-                    </div>
-                </div>
+            <div className="about-skill">
+              <p>Java</p>
+              <hr style={{ width: "70%" }} />
             </div>
+            <div className="about-skill">
+              <p>React.js</p>
+              <hr style={{ width: "40%" }} />
+            </div>
+            <div className="about-skill">
+              <p>MySQL</p>
+              <hr style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Spring Boot</p>
+              <hr style={{ width: "30%" }} />
+            </div>
+          </div>
         </div>
-        <div className="about-achievements">
-            <div className="about-achievement">
-                <h1>2+</h1>
-                <p>YEARS OF EXPERIENCE IN BUILDING APPS</p>
-            </div>
-            <hr />
-            <div className="about-achievement">
-                <h1>10+</h1>
-                <p>PROJECTS COMPLETED</p>
-            </div>
-            <hr />
-            <div className="about-achievement">
-                <h1>5+</h1>
-                <p>PROGRAMMING LANGUAGES</p>
-            </div>
+      </div>
+      <div className="about-links">
+        <a href="https://www.linkedin.com/in/yashwant-choure-514737231/" target="_blank" rel="noopener noreferrer">
+          <img src={linkedin_logo} alt="LinkedIn" />
+        </a>
+        <a href="https://leetcode.com/u/YashwantXDX/" target="_blank" rel="noopener noreferrer">
+          <img src={leetcode_logo} alt="LeetCode" />
+        </a>
+        <a href="https://www.geeksforgeeks.org/user/yashwantxdx/" target="_blank" rel="noopener noreferrer">
+          <img src={gfg_logo} alt="GeeksforGeeks" />
+        </a>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>2+</h1>
+          <p>YEARS OF EXPERIENCE IN BUILDING APPS</p>
         </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>5+</h1>
+          <p>PROGRAMMING LANGUAGES</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
